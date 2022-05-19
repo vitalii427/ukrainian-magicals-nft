ca5a1cfe4cd9537fbc890a47a75d3f4f2c8bdd46
vitalii427 <vitalii427@yahoo.com>
2022-05-19 23:44:40 +0300

    Add icon

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -15,6 +15,8 @@ NOTES:
   - To prevent the deployed contract from being modified or deleted, it should not have any access
     keys on its account.
 */
+mod icon;
+
 use near_contract_standards::non_fungible_token::events::NftMint;
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
@@ -26,6 +28,8 @@ use near_sdk::collections::LazyOption;
 use near_sdk::{
     env, near_bindgen, require, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
 };
+
+use crate::icon::DATA_IMAGE_PNG_NEAR_ICON;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -55,7 +59,8 @@ impl Contract {
             spec: NFT_METADATA_SPEC.to_owned(),
             name: "Ukrainian Magicals".to_owned(),
             symbol: "UAMAG".to_owned(),
-            icon: None, //Some(DATA_IMAGE_SVG_NEAR_ICON.to_owned()),
+
+            icon: Some(DATA_IMAGE_PNG_NEAR_ICON.to_owned()),
             base_uri: Some(ARWEAVE_GATEWAY_BASE_URL.to_owned()),
             reference: None,
             reference_hash: None,
@@ -168,7 +173,6 @@ mod tests {
 
     const MINT_STORAGE_COST: u128 = 5870000000000000000000;
     const MINT_ALL_STORAGE_COST: u128 = 15460000000000000000000;
-    //const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
     impl Contract {
         /// Mint a new token with ID=`token_id` belonging to `token_owner_id`.
