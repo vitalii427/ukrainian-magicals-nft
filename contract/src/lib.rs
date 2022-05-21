9be465d02c6f34263c2cf6b7a9753294264ac464
vitalii427 <vitalii427@yahoo.com>
2022-05-21 11:35:27 +0300

    cleanup

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -21,8 +21,9 @@ use near_contract_standards::non_fungible_token::events::NftMint;
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
-use near_contract_standards::non_fungible_token::{refund_deposit_to_account, NonFungibleToken};
-use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_contract_standards::non_fungible_token::{
+    refund_deposit_to_account, NonFungibleToken, Token, TokenId,
+};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
 use near_sdk::{
